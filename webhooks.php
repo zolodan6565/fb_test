7b7ca2aef96573df551eb437946803b303baeb34
zolodan6565 <40051920+zolodan6565@users.noreply.github.com>
2018-11-27 17:16:42 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -97,36 +97,20 @@ else{
 	}';
 }
 
-if ($message_to_reply === 'ก๊าบบ บอทน้อยงงอ่ะ ยังไงรอสักครู่ให้ทางแอดมินมาตอบนะครับ'){
-	//Encode the array into JSON.
-	$jsonDataEncoded = $jsonData;
-	//Tell cURL that we want to send a POST request.
-	curl_setopt($ch, CURLOPT_POST, 1);
-	//Attach our encoded JSON string to the POST fields.
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-	//Set the content type to application/json
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-	//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-	//Execute the request
-	if(!empty($input['entry'][0]['messaging'][0]['message'])){
-	    $result = curl_exec($ch);
-	}
-	sleep(15);
+
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+//Execute the request
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+    $result = curl_exec($ch);
 }
-else{
-	//Encode the array into JSON.
-	$jsonDataEncoded = $jsonData;
-	//Tell cURL that we want to send a POST request.
-	curl_setopt($ch, CURLOPT_POST, 1);
-	//Attach our encoded JSON string to the POST fields.
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-	//Set the content type to application/json
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-	//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-	//Execute the request
-	if(!empty($input['entry'][0]['messaging'][0]['message'])){
-	    $result = curl_exec($ch);
-	}
-}
+
 
 ?>
