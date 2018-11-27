4b217a220a5b926490c2e05a8a721af74e5b4611
zolodan6565 <40051920+zolodan6565@users.noreply.github.com>
2018-11-27 16:42:16 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -85,6 +85,78 @@ if (strpos($message, 'เว็บบริษัท') !== false) {
 	  }
 	}';
 }
+if (strpos($message, 'รายการสินค้า') !== false) {
+	$jsonData = '{
+		"recipient":{
+		"id":"'.$sender.'"
+	  }, 
+	  "message": {
+		"attachment": {
+		  "type": "template",
+		  "payload": {
+			"template_type": "list",
+			"top_element_style": "compact",
+			"elements": [
+			  {
+				"title": "Classic T-Shirt Collection",
+				"subtitle": "See all our colors",
+				"image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+				"buttons": [
+				  {
+					"title": "View",
+					"type": "web_url",
+					"url": "https://peterssendreceiveapp.ngrok.io/collection",
+					"messenger_extensions": true,
+					"webview_height_ratio": "tall",
+					"fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+				  }
+				]
+			  },
+			  {
+				"title": "Classic White T-Shirt",
+				"subtitle": "See all our colors",
+				"default_action": {
+				  "type": "web_url",
+				  "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+				  "messenger_extensions": false,
+				  "webview_height_ratio": "tall"
+				}
+			  },
+			  {
+				"title": "Classic Blue T-Shirt",
+				"image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+				"subtitle": "100% Cotton, 200% Comfortable",
+				"default_action": {
+				  "type": "web_url",
+				  "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+				  "messenger_extensions": true,
+				  "webview_height_ratio": "tall",
+				  "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+				},
+				"buttons": [
+				  {
+					"title": "Shop Now",
+					"type": "web_url",
+					"url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+					"messenger_extensions": true,
+					"webview_height_ratio": "tall",
+					"fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+				  }
+				]        
+			  }
+			],
+			 "buttons": [
+			  {
+				"title": "View More",
+				"type": "postback",
+				"payload": "payload"            
+			  }
+			]  
+		  }
+		}
+	  }
+	}';
+}
 else{
 	$jsonData = '{
 		"recipient":{
