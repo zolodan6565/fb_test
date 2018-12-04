19378825524b4c0f7c2f0335791699ce5ff7ad2b
zolodan6565 <40051920+zolodan6565@users.noreply.github.com>
2018-12-04 11:44:03 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -98,7 +98,7 @@ else if (strpos($message, 'ขอรูป') !== false) {
 		"attachment":{
 		  "type":"image",
 		  "payload":{
-		  	"text":"'.$message_to_reply.'",
+		  	
 			"url":"https://i.ytimg.com/vi/ErHo9uWC5_M/hqdefault.jpg",
 			"is_reusable":true,
 			
